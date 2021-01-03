4376ab73a52328b6a8530cc9c79b2e136465df94
Eugene Jang <jiniixz@naver.com>
2021-01-03 23:18:49 +0900

    finish step03 start step04

---
+++ DL_FrameWork/steps/step04.py
@@ -0,0 +1,24 @@
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+import numpy as np
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    def forward(self, x):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+class Exp(Function):
+    def forward(selfself, x):
+        return np.exp(x)
+
