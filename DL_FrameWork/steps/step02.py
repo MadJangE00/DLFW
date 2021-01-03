ba423fc5dd8d6800cacaf35be0a5c9aa8a109010
Eugene Jang <jiniixz@naver.com>
2021-01-03 23:10:50 +0900

    Initial commit

---
+++ DL_FrameWork/steps/step02.py
@@ -0,0 +1,25 @@
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+import numpy as np
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    def forward(self, x):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+x = Variable(np.array(10))
+f = Square()
+y = f(x)
+print(type(y))
+print(y.data)
