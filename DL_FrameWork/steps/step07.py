2fde43b344c38ad6e136e4354bd6404755e5d714
Eugene Jang <jiniixz@naver.com>
2021-01-06 22:26:34 +0900

    start step07 finish

--- DL_FrameWork/steps/step07.py
+++ DL_FrameWork/steps/step07.py
@@ -8,6 +8,13 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
+
 import numpy as np
 
 class Function:
@@ -52,3 +59,16 @@ def numerical_diff(f, x, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+#역전파
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
